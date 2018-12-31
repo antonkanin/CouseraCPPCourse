c1f89a8f27bed155f7d334d04ffbdc246a53f456
antonkanin <antonkanin@users.noreply.github.com>
2018-12-31 17:27:28 +0300

    Week 2 - Section 3 (Set) - Task 3 Synonyms

--- CourseraCPP/CourseraCPP.cpp
+++ CourseraCPP/CourseraCPP.cpp
@@ -10,49 +10,82 @@
 
 using namespace std;
 
-set<string> BuildMapValuesSet(const map<int, string>& m)
+void RunProgram(istream& inputStream)
 {
-	set<string> result;
-	for (const auto& item : m)
+	int linesCount;
+	inputStream >> linesCount;
+
+	set<set<string>> dictionary;
+
+	map<string, int> synonymCounter;
+
+	vector<string> result;
+
+	for (int lineIndex = 0; lineIndex < linesCount; ++lineIndex)
 	{
-		result.insert(item.second);
+		string command;
+		inputStream >> command;
+
+		if (command == "ADD")
+		{
+			string word1, word2;
+			inputStream >> word1 >> word2;
+
+			if (dictionary.count({ word1, word2 }) == 0)
+			{
+				synonymCounter[word1]++;
+				synonymCounter[word2]++;
+				dictionary.insert({ word1, word2 });
+			}
+			
+		}
+
+		else if (command == "COUNT")
+		{
+			string word;
+			inputStream >> word;
+
+			if (synonymCounter.count(word) > 0)
+			{
+				result.push_back(to_string(synonymCounter[word]));
+			}
+			else
+			{
+				result.push_back("0");
+			}
+		}
+
+		else if (command == "CHECK")
+		{
+			string word1, word2;
+			inputStream >> word1 >> word2;
+
+			if (dictionary.count({word1, word2}) > 0)
+			{
+				result.push_back("YES");
+			}
+			else
+			{
+				result.push_back("NO");
+			}
+		}
 	}
-	return result;
+
+	for (const auto& value : result)
+	{
+		cout << value << endl;
+	}
 }
 
-//void RunProgram(istream& inputStream)
-//{
-//	int linesCount;
-//	inputStream >> linesCount;
-//
-//	set<string> stringsSet;
-//
-//	vector<string> result;
-//
-//	for (int lineIndex = 0; lineIndex < linesCount; ++lineIndex)
-//	{
-//		string s;
-//		inputStream >> s;
-//		stringsSet.insert(s);
-//	}
-//
-//	cout << stringsSet.size();
-//
-//	//for (const auto& value : result)
-//	//{
-//	//	cout << value << endl;
-//	//}
-//}
+int main()
+{
+	//ifstream file("input.txt");
+	//if (file.is_open())
+	//{
+	//	RunProgram(file);
+	//	file.close();
+	//}
 
-//int main()
-//{
-//	/*ifstream file("input.txt");
-//	if (file.is_open())
-//	{
-//		RunProgram(file);
-//		file.close();
-//	}*/
-//
-//	RunProgram(cin);
-//}
+	RunProgram(cin);
+}
 
