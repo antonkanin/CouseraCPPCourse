e8f3a53c6baee5699c938d53c146ffac7f3c7403
antonkanin <antonkanin@users.noreply.github.com>
2018-12-31 12:43:31 +0300

    Week 2 - Section 2 (Map) - Task 5 Bus Stops #2

--- CourseraCPP/CourseraCPP.cpp
+++ CourseraCPP/CourseraCPP.cpp
@@ -11,123 +11,38 @@ using namespace std;
 
 void RunProgram(istream& inputStream)
 {
-	int count;
-	inputStream >> count;
+	int linesCount;
+	inputStream >> linesCount;
 
 	// mas of bus routes, each route is a vector of stops
-	map<string, vector<string>> busRoutes;
+	map<vector<string>, int> busRoutes;
 
-	// map of stops, each stop is a vector 
-	map<string, vector<string>> busStops;
-
-	map<string, string> capitals;
 	vector<string> result;
 
-	for (int i = 0; i < count; ++i)
+	for (int lineIndex = 0; lineIndex < linesCount; ++lineIndex)
 	{
-		string command;
-		inputStream >> command;
+		int numberOfStops;
+		inputStream >> numberOfStops;
 
-		if (command == "NEW_BUS")
+		vector<string> route;
+
+		for (int stopIndex = 0; stopIndex < numberOfStops; stopIndex++)
 		{
-			string bus;
-			inputStream >> bus;
-			
-			int stopsCount;
-			inputStream >> stopsCount;
+			string stopName;
+			inputStream >> stopName;
 
-			vector<string> stops;
-			for (int stopIndex = 0; stopIndex < stopsCount; ++stopIndex)
-			{
-				string stopName;
-				inputStream >> stopName;
-				stops.push_back(stopName);
-
-				// adding the bus route to the current stop
-				busStops[stopName].push_back(bus);
-			}
-
-			busRoutes[bus] = stops;
+			route.push_back(stopName);
 		}
 
-		else if (command == "BUSES_FOR_STOP")
+		if (busRoutes.count(route) == 0)
 		{
-			string stop;
-			inputStream >> stop;
-			if (busStops.count(stop) == 0)
-			{
-				result.push_back("No stop");
-			}
-			else
-			{
-				string busesList;
-				for (const auto& bus : busStops[stop])
-				{
-					busesList += bus + " ";
-				}
-				result.push_back(busesList);
-			}
-				
+			const int newSize = busRoutes.size() + 1;
+			busRoutes[route] = newSize;
+			result.push_back("New bus " + to_string(newSize));
 		}
-		else if (command == "STOPS_FOR_BUS")
+		else
 		{
-			string busInput;
-			inputStream >> busInput;
-			if (busRoutes.count(busInput) == 0)
-			{
-				result.push_back("No bus");
-			}
-			else
-			{
-				// go through each Bus Stop for a given bus
-				for (const auto& stop : busRoutes[busInput])
-				{
-					// if this stop has more then 1 buses
-					if (busStops[stop].size() > 1)
-					{
-						string resultLine = "Stop " + stop + ": ";
-						for (const auto& bus : busStops[stop])
-						{
-							if (bus != busInput)
-							{
-								resultLine += bus + " ";
-							}
-						}
-						result.push_back(resultLine);
-					}
-					else
-					{
-						result.push_back("Stop " + stop + ": no interchange");
-					}
-				}
-			}
-		}
-
-		else if (command == "ALL_BUSES")
-		{
-			//sort(busRoutes.begin(), busRoutes.end(), 
-			//	[](const string& a, const string& b) -> bool
-			//{
-			//	return a > b;
-			//});
-
-			if (busRoutes.size() == 0)
-			{
-				result.push_back("No buses");
-			}
-			else
-			{
-				for (const auto& route : busRoutes)
-				{
-					string resultLine = "Bus " + route.first + ": ";
-					for (const auto& stop : route.second)
-					{
-						resultLine += stop + " ";
-					}
-					result.push_back(resultLine);
-				}
-
-			}
+			result.push_back("Already exists for " + to_string(busRoutes[route]));
 		}
 	}
 
@@ -139,13 +54,21 @@ void RunProgram(istream& inputStream)
 
 int main()
 {
-	//ifstream file("input.txt");
-	//if (file.is_open())
-	//{
-	//	RunProgram(file);
-	//	file.close();
-	//}
+	/*ifstream file("input.txt");
+	if (file.is_open())
+	{
+		RunProgram(file);
+		file.close();
+	}*/
 
-	RunProgram(cin);
+	//RunProgram(cin);
+	map<int, string> m = { {1, "odd"}, {2, "even"}, {1, "one"} };
+	m[2] = "two";
+	m.erase(1);
+	m[3] = "three";
+	m.erase(4);
+	m[5] = "five";
+	cout << m.size();
+
 }
 
