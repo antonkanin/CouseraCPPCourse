a296906449cbfbd7959ffc8adeabf5e323d742f0
antonkanin <antonkanin@users.noreply.github.com>
2018-12-31 15:06:32 +0300

    Week 2 - Section 3 (Set) - Task 3 Synonyms

--- CourseraCPP/CourseraCPP.cpp
+++ CourseraCPP/CourseraCPP.cpp
@@ -1,4 +1,4 @@
-#include "pch.h"
+//#include "pch.h"
 
 #include <iostream>
 #include <string>
@@ -6,69 +6,53 @@
 #include <map>
 #include <fstream>
 #include <algorithm>
+#include <set>
 
 using namespace std;
 
-void RunProgram(istream& inputStream)
+set<string> BuildMapValuesSet(const map<int, string>& m)
 {
-	int linesCount;
-	inputStream >> linesCount;
-
-	// mas of bus routes, each route is a vector of stops
-	map<vector<string>, int> busRoutes;
-
-	vector<string> result;
-
-	for (int lineIndex = 0; lineIndex < linesCount; ++lineIndex)
+	set<string> result;
+	for (const auto& item : m)
 	{
-		int numberOfStops;
-		inputStream >> numberOfStops;
-
-		vector<string> route;
-
-		for (int stopIndex = 0; stopIndex < numberOfStops; stopIndex++)
-		{
-			string stopName;
-			inputStream >> stopName;
-
-			route.push_back(stopName);
-		}
-
-		if (busRoutes.count(route) == 0)
-		{
-			const int newSize = busRoutes.size() + 1;
-			busRoutes[route] = newSize;
-			result.push_back("New bus " + to_string(newSize));
-		}
-		else
-		{
-			result.push_back("Already exists for " + to_string(busRoutes[route]));
-		}
+		result.insert(item.second);
 	}
-
-	for (const auto& value : result)
-	{
-		cout << value << endl;
-	}
+	return result;
 }
 
-int main()
-{
-	/*ifstream file("input.txt");
-	if (file.is_open())
-	{
-		RunProgram(file);
-		file.close();
-	}*/
+//void RunProgram(istream& inputStream)
+//{
+//	int linesCount;
+//	inputStream >> linesCount;
+//
+//	set<string> stringsSet;
+//
+//	vector<string> result;
+//
+//	for (int lineIndex = 0; lineIndex < linesCount; ++lineIndex)
+//	{
+//		string s;
+//		inputStream >> s;
+//		stringsSet.insert(s);
+//	}
+//
+//	cout << stringsSet.size();
+//
+//	//for (const auto& value : result)
+//	//{
+//	//	cout << value << endl;
+//	//}
+//}
 
-	//RunProgram(cin);
-	map<int, string> m = { {1, "odd"}, {2, "even"}, {1, "one"} };
-	m[2] = "two";
-	m.erase(1);
-	m[3] = "three";
-	m.erase(4);
-	m[5] = "five";
-	cout << m.size();
-
-}
+//int main()
+//{
+//	/*ifstream file("input.txt");
+//	if (file.is_open())
+//	{
+//		RunProgram(file);
+//		file.close();
+//	}*/
+//
+//	RunProgram(cin);
+//}
 
