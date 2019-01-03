93b67b1d6aed12e8e73715dcdc22355b615ef4be
antonkanin <antonkanin@users.noreply.github.com>
2019-01-03 13:17:36 +0300

    Week 4 - Section 2 - Task 4 (students list)

--- CourseraCPP/CourseraCPP.cpp
+++ CourseraCPP/CourseraCPP.cpp
@@ -5,30 +5,89 @@
 #include <string>
 #include <iomanip>
 #include <set>
+#include <vector>
 
 using namespace std;
 
-int main() 
+struct Student
 {
-	ifstream file("input.txt");
-	int height, width;
-	file >> height >> width;
-	file.ignore();
+	string firstName;
+	string lastName;
+	int day;
+	int month;
+	int year;
+};
 
-	string value;
-	for (int row = 0; row < height; ++row)
+void DoWork(istream& inputStream)
+{
+	int studentsCount;
+	inputStream >> studentsCount;
+
+	vector<Student> students;
+
+	for (int i = 0; i < studentsCount; ++i)
 	{
-		for (int col = 0; col < width - 1; ++col)
+		Student student;
+		inputStream >> student.firstName >> student.lastName >> student.day >> student.month >> student.year;
+
+		const int MAX_VAL = 1000000000;
+		if (student.day > MAX_VAL || student.month > MAX_VAL || student.year > MAX_VAL)
 		{
-			getline(file, value, ',');
-			int intValue = stoi(value);
-			cout << setw(10) << intValue << ' ';
+			cout << "bad request" << endl;
+		}
+		else
+		{
+			students.push_back(student);
+		}
+	}
+
+	int outputCount;
+	inputStream >> outputCount;
+
+	for (int i = 0; i < outputCount; ++i)
+	{
+		string operation;
+		string studentPositionStr;
+		inputStream >> operation >> studentPositionStr;
+
+		int studentPosition = 0;
+
+		try
+		{
+			studentPosition = stoi(studentPositionStr);
+		}
+		catch (std::invalid_argument)
+		{
+			cout << "bad request" << endl;
+			continue;
 		}
 		
-		getline(file, value);
-		int intValue = stoi(value);
-		cout << setw(10) << intValue << endl;
+
+		if (studentPosition < 1 || studentPosition > students.size())
+		{
+			cout << "bad request" << endl;
+			continue;
+		}
+
+		if (operation == "name")
+		{
+			cout << students[studentPosition - 1].firstName << " " << students[studentPosition - 1].lastName << endl;
+		}
+		else if (operation == "date")
+		{
+			cout << students[studentPosition - 1].day << "." << students[studentPosition - 1].month << "." << students[studentPosition - 1].year << endl;
+		}
+		else
+		{
+			cout << "bad request" << endl;
+		}
 	}
-	
-	return 0;
+}
+
+int main() 
+{
+	//ifstream file("input.txt");
+	//DoWork(file);
+
+	DoWork(cin);
 }
