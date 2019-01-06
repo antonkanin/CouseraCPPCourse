3dc2911eee89ab527b710552e6496e25f0efcc04
antonkanin <antonkanin@users.noreply.github.com>
2019-01-07 00:43:52 +0300

    Yellow Belt - Week1 - Section 4 -  Task 2 Task Tracker

--- CourseraCPP/CourseraCPP.cpp
+++ CourseraCPP/CourseraCPP.cpp
@@ -2,114 +2,150 @@
 #include <iostream>
 #include <map>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-//enum class Lang
+//enum class TaskStatus
 //{
-//	DE, FR, IT
+//	NEW,
+//	IN_PROGRESS,
+//	TESTING,
+//	DONE
 //};
 //
-//struct Region
-//{
-//	string std_name;
-//	string parent_std_name;
-//	map<Lang, string> names;
-//	int64_t population;
-//};
+//using TasksInfo = map<TaskStatus, int>;
 
-bool operator<(const Region& lhs, const Region& rhs)
+TaskStatus GetNextStatus(const TaskStatus& status)
 {
-	return 	tie(lhs.parent_std_name, lhs.std_name, lhs.names, lhs.population) <
-		tie(rhs.parent_std_name, rhs.std_name, rhs.names, rhs.population);
-}
-
-int FindMaxRepetitionCount(const vector<Region>& regions)
-{
-	using pair_type = map<Region, int>::value_type;
-
-	map<Region, int> regionsCount;
-	for (const auto& region : regions)
+	switch (status)
 	{
-		regionsCount[region]++;
-	}
-
-	const auto it = max_element(begin(regionsCount), end(regionsCount), [](pair_type a, pair_type b)
-	{
-		return a.second < b.second;
-	});
-
-	if (it != end(regionsCount))
-	{
-		return it->second;
-	}
-	else
-	{
-		return 0;
+		case TaskStatus::NEW:
+			return TaskStatus::IN_PROGRESS;
+			break;
+		case TaskStatus::IN_PROGRESS:
+			return TaskStatus::TESTING;
+			break;
+		case TaskStatus::TESTING:
+			return TaskStatus::DONE;
+			break;
+		default:
+			return status;
 	}
 }
 
+class TeamTasks
+{
+public:
+	const TasksInfo& GetPersonTasksInfo(const string& person) const
+	{
+		return personsTasks.at(person);
+	}
+
+	void AddNewTask(const string& person)
+	{
+		if (person.empty())
+		{
+			return;
+		}
+
+		personsTasks[person][TaskStatus::NEW]++;
+	}
+
+	tuple<TasksInfo, TasksInfo> PerformPersonTasks(
+		const string& person, int task_count)
+	{
+		if (personsTasks.count(person) == 0)
+		{
+			return { {}, {} };
+		}
+
+		TasksInfo allTasks = personsTasks.at(person);
+		TasksInfo updatedTasks;
+		TasksInfo oldTasks;
+
+		for (const auto& status : personsTasks.at(person))
+		{
+			if (status.first == TaskStatus::DONE)
+			{
+				break;
+			}
+
+			if (task_count > status.second)
+			{
+				allTasks[status.first] -= status.second;
+				allTasks[GetNextStatus(status.first)] += status.second;
+
+				updatedTasks[GetNextStatus(status.first)] += status.second;
+
+				task_count -= status.second;
+			}
+			else
+			{
+				allTasks[status.first] -= task_count;
+				allTasks[GetNextStatus(status.first)] += task_count;
+
+				updatedTasks[GetNextStatus(status.first)] += task_count;
+
+				if (task_count < status.second)
+				{
+					oldTasks[status.first] = status.second - task_count;
+				}
+
+				auto remainingStatus = GetNextStatus(status.first);
+
+				while (remainingStatus != TaskStatus::DONE && personsTasks.at(person).count(remainingStatus) > 0)
+				{
+					oldTasks[remainingStatus] = personsTasks.at(person).at(remainingStatus);
+					remainingStatus = GetNextStatus(remainingStatus);
+				}
+
+				break;
+			}
+		}
+
+		personsTasks[person] = allTasks;
+
+		return make_tuple(updatedTasks, oldTasks);
+	}
+
+private:
+	map<string, TasksInfo> personsTasks;
+};
+
+void PrintTasksInfo(TasksInfo tasks_info)
+{
+	//cout << tasks_info[TaskStatus::NEW] << " new tasks" <<
+	//	", " << tasks_info[TaskStatus::IN_PROGRESS] << " tasks in progress" <<
+	//	", " << tasks_info[TaskStatus::TESTING] << " tasks are being tested" <<
+	//	", " << tasks_info[TaskStatus::DONE] << " tasks are done" << endl;
+
+	cout << tasks_info[TaskStatus::NEW] << " " <<
+	tasks_info[TaskStatus::IN_PROGRESS] << " " <<
+	tasks_info[TaskStatus::TESTING] << " " <<
+	tasks_info[TaskStatus::DONE] << endl;
+}
 
 int main()
 {
-	cout << FindMaxRepetitionCount({
-	  {
-		  "Moscow",
-		  "Russia",
-		  {{Lang::DE, "Moskau"}, {Lang::FR, "Moscou"}, {Lang::IT, "Mosca"}},
-		  89
-	  }, {
-		  "Russia",
-		  "Eurasia",
-		  {{Lang::DE, "Russland"}, {Lang::FR, "Russie"}, {Lang::IT, "Russia"}},
-		  89
-	  }, {
-		  "Moscow",
-		  "Russia",
-		  {{Lang::DE, "Moskau"}, {Lang::FR, "Moscou"}, {Lang::IT, "Mosca"}},
-		  89
-	  }, {
-		  "Moscow",
-		  "Russia",
-		  {{Lang::DE, "Moskau"}, {Lang::FR, "Moscou"}, {Lang::IT, "Mosca"}},
-		  89
-	  }, {
-		  "Russia",
-		  "Eurasia",
-		  {{Lang::DE, "Russland"}, {Lang::FR, "Russie"}, {Lang::IT, "Russia"}},
-		  89
-	  },
-		}) << endl;
+	TeamTasks tasks;
 
-	cout << FindMaxRepetitionCount({
-		{
-			"Moscow",
-			"Russia",
-			{{Lang::DE, "Moskau"}, {Lang::FR, "Moscou"}, {Lang::IT, "Mosca"}},
-			89
-		}, {
-			"Russia",
-			"Eurasia",
-			{{Lang::DE, "Russland"}, {Lang::FR, "Russie"}, {Lang::IT, "Russia"}},
-			89
-		}, {
-			"Moscow",
-			"Russia",
-			{{Lang::DE, "Moskau"}, {Lang::FR, "Moscou deux"}, {Lang::IT, "Mosca"}},
-			89
-		}, {
-			"Moscow",
-			"Toulouse",
-			{{Lang::DE, "Moskau"}, {Lang::FR, "Moscou"}, {Lang::IT, "Mosca"}},
-			89
-		}, {
-			"Moscow",
-			"Russia",
-			{{Lang::DE, "Moskau"}, {Lang::FR, "Moscou"}, {Lang::IT, "Mosca"}},
-			31
-		},
-		}) << endl;
+	for (int i = 0; i < 3; ++i)
+	{
+		tasks.AddNewTask("Ivan");
+	}
+
+	TasksInfo updated_tasks, untouched_tasks;
+
+	for (int i = 0; i < 10; i++)
+	{
+		tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Ivan", 2);
+
+		PrintTasksInfo(tasks.GetPersonTasksInfo("Ivan"));
+		PrintTasksInfo(updated_tasks);
+		PrintTasksInfo(untouched_tasks);
+
+		cout << endl;
+	}
 
 	return 0;
 }
