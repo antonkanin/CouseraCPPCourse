cf05544b5bfb8cbc73fb6264c630db0be601822d
antonkanin <antonkanin@users.noreply.github.com>
2019-01-07 09:47:25 +0300

    Yellow Belt - Week1 - Section 5 Templates -  Task 2 Element Reference

--- CourseraCPP/CourseraCPP.cpp
+++ CourseraCPP/CourseraCPP.cpp
@@ -1,67 +1,27 @@
 //#include "pch.h"
 #include <iostream>
 #include <map>
-#include <vector>
+#include <string>
 
 using namespace std;
 
-template<typename T> T Sqr(T value);
-template<typename T> vector<T> Sqr(vector<T> value);
-template<typename T1, typename T2> pair<T1, T2> Sqr(pair<T1, T2> p);
-template<typename Key, typename Value> map<Key, Value> Sqr(map<Key, Value> m);
-
-template<typename T>
-T Sqr(T value)
+template<typename Key, typename Value>
+Value& GetRefStrict(map<Key, Value>& m, const Key& key)
 {
-	return value * value;
-}
-
-template<typename T>
-vector<T> Sqr(vector<T> value)
-{
-	for (auto& item : value)
+	if (m.count(key) == 0)
 	{
-		item = Sqr(item);
+		throw runtime_error("Key does not exist");
 	}
-	return value;
-}
 
-template<typename T1, typename T2>
-pair<T1, T2> Sqr(pair<T1, T2> p)
-{
-	return { Sqr(p.first), Sqr(p.second) };
-}
-
-template<typename Key, typename Value> 
-map<Key, Value> Sqr(map<Key, Value> m)
-{
-	for (auto& item : m)
-	{
-		item.second = Sqr(item.second);
-	}
-	return m;
+	return m[key];
 }
 
 int main()
 {
-	vector<int> v = { 1, 2, 3 };
-	cout << "vector:";
-	for (int x : Sqr(v))
-	{
-		cout << ' ' << x;
-	}
-	cout << endl;
-
-	map<int, pair<int, int>> map_of_pairs = {
-	  {4, {2, 2}},
-	  {7, {4, 3}}
-	};
-
-	cout << "map of pairs:" << endl;
-	for (const auto& x : Sqr(map_of_pairs))
-	{
-		cout << x.first << ' ' << x.second.first << ' ' << x.second.second << endl;
-	}
-
+	map<int, string> m = { {0, "value"} };
+	string& item = GetRefStrict(m, 0);
+	item = "newvalue";
+	cout << m[0] << endl; 
+	
 	return 0;
 }
