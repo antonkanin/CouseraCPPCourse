2080ce1b5b0f19a1b3cbba08a6de46fd96b500bb
antonkanin <antonkanin@users.noreply.github.com>
2019-01-12 23:33:15 +0300

    Yellow Belt - Week 5 - Section 2 Polymorphism - Task 3 Peer Review (before)

--- CourseraCPP/main.cpp
+++ CourseraCPP/main.cpp
@@ -2,62 +2,151 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <queue>
+#include <iomanip>
+#include <sstream>
+#include <cmath>
+#include <memory>
 
 using namespace std;
 
-bool IsHigherPriority(const char& prev, const char& next)
+class Figure
 {
-	return (next == '*' || next == '/') && (prev == '+' || prev == '-');
+public:
+	virtual ~Figure() = default;
+
+	virtual string Name() const = 0;
+	virtual double Perimeter() const = 0;
+	virtual double Area() const = 0;
+};
+
+class Triangle : public Figure
+{
+public:
+	Triangle(float a, float b, float c) : a(a), b(b), c(c)
+	{}
+
+	string Name() const override
+	{
+		return "TRIANGLE";
+	}
+
+	double Perimeter() const override
+	{
+		return a + b + c;
+	}
+
+	double Area() const override
+	{
+		double p = (a + b + c) / 2;
+		return sqrt(p * (p - a) * (p - b) * (p - c));
+	}
+
+private:
+	double a, b, c;
+};
+
+class Rect : public Figure
+{
+public:
+	Rect(const float& a, const float& b) : a(a), b(b) {}
+
+	string Name() const override
+	{
+		return "RECT";
+	}
+
+	double Perimeter() const override 
+	{
+		return 2 * (a + b);
+	}
+
+	double Area() const override
+	{
+		return a * b;
+	}
+
+private:
+	float a, b;
+};
+
+class Circle : public Figure
+{
+public:
+	Circle(double r) : r(r) {}
+
+	string Name() const override
+	{
+		return "CIRCLE";
+	}
+
+	double Perimeter() const override
+	{
+		return 2 * PI * r;
+	}
+
+	double Area() const override
+	{
+		return PI * r * r;
+	}
+
+private:
+	float r;
+
+	const double PI = 3.14;
+};
+
+shared_ptr<Figure> CreateFigure(istream& inputStream)
+{
+	string type;
+	inputStream >> type;
+
+	if (type == "RECT")
+	{
+		float a, b;
+		inputStream >> a >> b;
+		return make_shared<Rect>(a, b);
+	} 
+	
+	if (type == "TRIANGLE")
+	{
+		float a, b, c;
+		inputStream >> a >> b >> c;
+		return make_shared<Triangle>(a, b, c);
+	}
+
+	if (type == "CIRCLE")
+	{
+		float r;
+		inputStream >> r;
+		return make_shared<Circle>(r);
+	}
 }
 
 int main()
 {
-	string originalNumber;
-	cin >> originalNumber;
-
-	int operationsCount;
-	cin >> operationsCount;
-
-	queue<pair<char, string>> operations;
-	char prev_operation;
-	char operation = ' ';
-	int bracketsCounter = 0;
-
-	while (operationsCount > 0)
+	vector<shared_ptr<Figure>> figures;
+	for (string line; getline(cin, line); )
 	{
-		prev_operation = operation;
-		string number;
-		cin >> operation >> number;
-		operations.push({ operation, number });
-		if (IsHigherPriority(prev_operation, operation))
+		istringstream is(line);
+
+		string command;
+		is >> command;
+
+		if (command == "ADD")
 		{
-			++bracketsCounter;
+			figures.push_back(CreateFigure(is));
 		}
 
-		--operationsCount;
-	}
-
-	for (int i = 0; i < bracketsCounter; ++i)
-	{
-		cout << "(";
-	}
-
-	cout << originalNumber;
-
-	operation = ' ';
-	while (!operations.empty())
-	{
-		prev_operation = operation;
-		auto& op = operations.front();
-		operation = op.first;
-		if (IsHigherPriority(prev_operation, operation))
+		else if (command == "PRINT")
 		{
-			cout << ")";
+			for (const auto& current_figure : figures)
+			{
+				cout << fixed << setprecision(3)
+					<< current_figure->Name() << " "
+					<< current_figure->Perimeter() << " "
+					<< current_figure->Area() << endl;
+			}
 		}
-		cout << " " << operation << " " << op.second;
-		operations.pop();
 	}
-
 	return 0;
 }
